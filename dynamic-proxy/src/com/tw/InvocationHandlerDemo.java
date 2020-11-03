22a3acdb19eb8a0881f366df1b6ae42c3b52d95e
Jia Wei <jiawei.hello@gmail.com>
2020-11-03 23:09:11 +0800

    refactor

--- dynamic-proxy/src/com/tw/InvocationHandlerDemo.java
+++ dynamic-proxy/src/com/tw/InvocationHandlerDemo.java
@@ -5,11 +5,11 @@ import java.lang.reflect.Method;
 
 public class InvocationHandlerDemo implements InvocationHandler {
     // 这个就是我们要代理的真实对象
-    private Object subject;
+    private Object object;
 
     // 构造方法，给我们要代理的真实对象赋初值
-    public InvocationHandlerDemo(Object subject) {
-        this.subject = subject;
+    public InvocationHandlerDemo(Object object) {
+        this.object = object;
     }
 
     @Override
@@ -18,7 +18,7 @@ public class InvocationHandlerDemo implements InvocationHandler {
         System.out.println("Before method");
         System.out.println("Call Method: " + method);
         // 当代理对象调用真实对象的方法时，其会自动的跳转到代理对象关联的handler对象的invoke方法来进行调用
-        Object obj = method.invoke(subject, args);
+        Object obj = method.invoke(this.object, args);
         // 在代理真实对象后我们也可以添加一些自己的操作
         System.out.println("After method");
         System.out.println();
--- dynamic-proxy/src/com/tw/Subject.java
+++ dynamic-proxy/src/com/tw/Subject.java
@@ -2,5 +2,6 @@ package com.tw;
 
 public interface Subject {
     void hello(String str);
+
     String bye();
 }
